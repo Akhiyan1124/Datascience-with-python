6e5d10771c9db30fe04ad8bc97a9d454623fb98c
Muhammad Akhtar <154078260+Akhiyan1124@users.noreply.github.com>
2026-02-16 13:12:44 +0500

    Create error_examples.py

---
+++ lab1/error_examples.py
@@ -0,0 +1,12 @@
+# error_examples.py
+
+# Syntax Error Example
+# print("Hello World"
+
+# Type Error Example
+# result = "10" + 5
+
+# Name Error Example
+# print(age)
+
+print("Error examples are commented to prevent program crash.")
