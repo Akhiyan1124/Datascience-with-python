d60e0d0ae192d1925439484b63c515ec5c7e1b63
Muhammad Akhtar <154078260+Akhiyan1124@users.noreply.github.com>
2026-02-16 13:07:36 +0500

    Create variable_naming.py

---
+++ lab1/variable_naming.py
@@ -0,0 +1,16 @@
+# variable_naming.py
+# Rules for naming variables
+
+# Valid variable names
+student_name = "Ali"
+_age = 20
+totalMarks = 450
+
+print(student_name)
+print(_age)
+print(totalMarks)
+
+# Invalid examples (commented to avoid error)
+# 2name = "Wrong"      # Cannot start with number
+# my-name = "Wrong"    # Cannot use hyphen
+# class = "Wrong"      # Reserved keyword
