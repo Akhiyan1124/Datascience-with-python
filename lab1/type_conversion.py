940df2ca435ca5632a57f8614da0d9e26792395f
Muhammad Akhtar <154078260+Akhiyan1124@users.noreply.github.com>
2026-02-16 13:10:30 +0500

    Create type_conversion.py

---
+++ lab1/type_conversion.py
@@ -0,0 +1,13 @@
+# type_conversion.py
+
+num1 = "10"
+num2 = "5"
+
+# Convert string to integer
+num1 = int(num1)
+num2 = int(num2)
+
+result = num1 + num2
+
+print("Sum:", result)
+print("Type of result:", type(result))
