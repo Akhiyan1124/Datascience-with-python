ad600f975e37861520c817e1884a88740b8181b1
Muhammad Akhtar <154078260+Akhiyan1124@users.noreply.github.com>
2026-02-16 13:05:52 +0500

    Create variables_demo.py

---
+++ lab1/variables_demo.py
@@ -0,0 +1,26 @@
+# variables_demo.py
+# Demonstration of Python variables and data types
+
+# Integer
+age = 25
+
+# Float
+height = 5.9
+
+# String
+name = "Akhtar"
+
+# Boolean
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
+
+# Checking data types
+print("\nData Types:")
+print("Type of name:", type(name))
+print("Type of age:", type(age))
+print("Type of height:", type(height))
+print("Type of is_student:", type(is_student))
