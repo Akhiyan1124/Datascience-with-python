d252cc50d1492049d4e1528eafa08a52121c9cdc
Muhammad Akhtar <154078260+Akhiyan1124@users.noreply.github.com>
2026-02-16 13:09:51 +0500

    Create advanced_input.py

---
+++ lab1/advanced_input.py
@@ -0,0 +1,10 @@
+# advanced_input.py
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+height = float(input("Enter your height in meters: "))
+
+print("\nUser Information")
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
